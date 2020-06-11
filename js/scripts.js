d2ef2fbbc012aa465c8f300c3faae5ed9127c12f
Ruthie Newman <ruthienewman@protonmail.com>
2020-06-11 11:43:55 -0700

    add function to loop through user input, divide user input and set the output to its equivalent Roman numeral symbol
    Co-authored-by: Kyle Hubbard <kyle.james.hubbard@gmail.com>, Co-authored-by: Sara Kane <kane.sarak@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,72 @@
 $(document).ready(function(){
   $("form#numberInput").submit(function(event){
     event.preventDefault();
-    const userNumber = parseInt($("input#numberInput").val());
-    
+    let userNumber = parseInt($("input#userNumber").val());
+    $(".outputNumber").text(translateToRomanNumeral(userNumber));
   
 
     
   })
 })
+
+function translateToRomanNumeral(number) {
+  let remainder = 0;
+  let result;
+
+  // romanNumeralM(number);
+  // romanNumeralD(number);
+  // romanNumeralC(number);
+  // romanNumeralL(number);
+  // romanNumeralX(number);
+  // romanNumeralV(number);
+  //romanNumeralI(number);
+
+  result = romanNumeralI(number);
+  return result;
+}
+
+
+
+
+
+function romanNumeralI(number) {
+  const dividedNumber = number/1
+  return addI(dividedNumber)
+}
+
+function addI(number) {
+  let iReturnString = "";
+  for (i=0; i < number; i++) {
+    iReturnString += "I"
+  }
+
+  return iReturnString
+}
+
+
+/* 
+
+for (i=0; i < number.length; i++) {
+
+  extractedNumber = number.charAt(number.length - i)
+
+  divisibleNumber = extractedNumber + zero(i)
+}
+
+function zero(number) {
+
+  if (0) {
+    return ""
+  } else if (1) {
+    return "0"
+  } else if (2)
+}
+
+*/
+
+
+
+
 
 
 
@@ -28,8 +87,6 @@ romanNumeralC (53) {
   53/100 = 0.1232321
 
   0
-
-
 }
 
 romanNumeralL (53)
